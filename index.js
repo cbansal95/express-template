d2b5fe38bcb226a4b29d9937278c97a559655081
Chirag Bansal <cbansal95@gmail.com>
2024-01-25 18:25:04 +0530

    initial boilerplate

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const sqlite = require('sqlite')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
