95ef8f1923ef97a4577faccc3ca114530519d39b
Chirag Bansal <cbansal95@gmail.com>
2024-01-27 20:20:25 +0530

    user object validation

--- helpers/schemaValidation.js
+++ helpers/schemaValidation.js
@@ -1,14 +1,30 @@
 const Joi = require('joi')
 
 const idSchema = Joi.number().greater(0)
+const usernameSchema = Joi.string().min(2)
+const useremailSchema = Joi.string().email()
+const userObjSchema = Joi.object({
+    id: idSchema.required(),
+    name: usernameSchema.required(),
+    email: useremailSchema.required()
+})
 
 function validateID(id) {
     try {
         Joi.assert(id, idSchema)
         return true
     } catch (err) {
-        return err.details[0].message
+        return "Invalid ID: " + err.details[0].message
     }
 }
 
-module.exports = {validateID}
+function validateUserObject(userObj) {
+    try{
+        Joi.assert(userObj, userObjSchema)
+        return true
+    } catch (err) {
+        return "Invalid user object: " + err.details[0].message
+    }
+}
+
+module.exports = {validateID, validateUserObject}
